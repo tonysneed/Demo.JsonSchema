66a6f1f5a286f97db9c7a2f891b08ecf1caaa0e5
Anthony Sneed <anthony.sneed@hilti.com>
2021-04-27 18:59:09 -0500

    Initial commit.

---
+++ Person.cs
@@ -0,0 +1,9 @@
+namespace Demo.JsonSchema
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        // public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
+
+namespace Demo.JsonSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new JSchemaGenerator
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                SchemaIdGenerationHandling = SchemaIdGenerationHandling.TypeName
+            };
+            var schema1 = generator.Generate(typeof(Person));
+
+            var schemaJson = @"{
+  '$id': 'Person',
+  'type': 'object',
+  'properties': {
+    'name': {
+      'type': [
+        'string',
+        'null'
+      ]
+    },
+    'age': {
+      'type': 'integer'
+    }
+  }
+}";
+            var schema = JSchema.Parse(schemaJson);
+            Console.WriteLine("Person Schema:");
+            Console.WriteLine(schema);
+
+            var person = new Person {Name = "Yoda", City = "Degoba"};
+            var settings = new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()};
+            var json = JsonConvert.SerializeObject(person, Formatting.Indented, settings);
+            Console.WriteLine("\nPerson JSON:");
+            Console.WriteLine(json);
+
+            var jobj = JObject.Parse(json);
+            var valid = jobj.IsValid(schema, out IList<string> errorMessages);
+            Console.WriteLine($"\nIs Valid: {valid}");
+            if (!valid)
+            {
+                foreach (var errorMessage in errorMessages)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            }
+        }
+    }
+}
